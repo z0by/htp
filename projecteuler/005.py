64edf02b81a95a265029bc1a4eb4df46d880a85b
z0by <r00t@z0.by>
2015-07-30 17:23:48 +0300

    Smallest multiple

---
+++ projecteuler/005.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+Smallest multiple
+Problem 5
+-------------------------
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+"""
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+def lcmm(*args):
+    return reduce(lcm, args)
+
+
+if __name__ == "__main__":
+    print lcmm(*range(1,21))
